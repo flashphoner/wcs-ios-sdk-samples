b21820cf134020bb1737241652f62bdca49fdaa2
flashphoner <support@flashphoner.com>
2016-11-28 11:57:01 +0700

    Add StreamRecordinf example

---
+++ WCSExample/StreamRecording/ViewController.h
@@ -0,0 +1,34 @@
+//
+//  ViewController.h
+//  WCSApiExample
+//
+//  Created by user on 24/11/2015.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FPWCSApi2/RTCEAGLVideoView.h>
+@import AVFoundation;
+@import AVKit;
+
+@interface ViewController : UIViewController<UITextFieldDelegate, RTCEAGLVideoViewDelegate, AVAssetResourceLoaderDelegate>
+
+@property UIScrollView *scrollView;
+@property UIView *contentView;
+
+@property UITextField *connectUrl;
+@property UILabel *status;
+@property UIButton *startButton;
+
+@property UITextView *recordLink;
+
+@property UIView *videoContainer;
+@property(nonatomic) RTCEAGLVideoView *remoteDisplay;
+
+@property NSMutableArray *remoteDisplayConstraints;
+
+//@property (strong, nonatomic) AVPlayerViewController *playerViewController;
+//@property (strong, nonatomic) AVPlayer *player;
+
+@end
+
---
+++ WCSExample/StreamRecording/ViewController.m
@@ -0,0 +1,422 @@
+//
+//  ViewController.m
+//  WCSApiExample
+//
+//  Created by user on 24/11/2015.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import "WCSUtil.h"
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <FPWCSApi2/FPWCSApi2.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+NSURL *url;
+NSString *streamName;
+NSString *recordName;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupViews];
+    [self setupLayout];
+    [self onDisconnected];
+    NSLog(@"Did load views");
+}
+
+//connect
+- (FPWCSApi2Session *)connect {
+    FPWCSApi2SessionOptions *options = [[FPWCSApi2SessionOptions alloc] init];
+    url =[[NSURL alloc] initWithString:_connectUrl.text];
+    options.urlServer = [NSString stringWithFormat:@"%@://%@:%@", url.scheme, url.host, url.port];
+    streamName = [url.path.stringByDeletingPathExtension stringByReplacingOccurrencesOfString: @"/" withString:@""];
+    options.appKey = @"defaultApp";
+    NSError *error;
+    FPWCSApi2Session *session = [FPWCSApi2 createSession:options error:&error];
+    if (!session) {
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failed to connect"
+                                     message:error.localizedDescription
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:@"Ok"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       [self onDisconnected];
+                                   }];
+        
+        [alert addAction:okButton];
+        [self presentViewController:alert animated:YES completion:nil];
+        return nil;
+    }
+    
+    [session on:kFPWCSSessionStatusEstablished callback:^(FPWCSApi2Session *rSession){
+        [self changeConnectionStatus:[rSession getStatus]];
+        [self onConnected:rSession];
+    }];
+    
+    [session on:kFPWCSSessionStatusDisconnected callback:^(FPWCSApi2Session *rSession){
+        [self changeConnectionStatus:[rSession getStatus]];
+        [self onDisconnected];
+    }];
+    
+    [session on:kFPWCSSessionStatusFailed callback:^(FPWCSApi2Session *rSession){
+        [self changeConnectionStatus:[rSession getStatus]];
+        [self onDisconnected];
+    }];
+    [session connect];
+    return session;
+}
+
+- (FPWCSApi2Stream *)publishStream {
+    FPWCSApi2Session *session = [FPWCSApi2 getSessions][0];
+    FPWCSApi2StreamOptions *options = [[FPWCSApi2StreamOptions alloc] init];
+    options.name = streamName;
+    options.display = _remoteDisplay;
+    options.record = true;
+    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ) {
+        options.constraints = [[FPWCSApi2MediaConstraints alloc] initWithAudio:YES videoWidth:640 videoHeight:480 videoFps:15];
+    }
+    NSError *error;
+    FPWCSApi2Stream *stream = [session createStream:options error:&error];
+    if (!stream) {
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failed to publish"
+                                     message:error.localizedDescription
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:@"Ok"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       [self onUnpublished];
+                                   }];
+        
+        [alert addAction:okButton];
+        [self presentViewController:alert animated:YES completion:nil];
+        return nil;
+    }
+    [stream on:kFPWCSStreamStatusPublishing callback:^(FPWCSApi2Stream *rStream){
+        [self changeStreamStatus:rStream];
+        [self onPublishing:rStream];
+    }];
+    
+    [stream on:kFPWCSStreamStatusUnpublished callback:^(FPWCSApi2Stream *rStream){
+        [self changeStreamStatus:rStream];
+        [self onUnpublished];
+    }];
+    
+    [stream on:kFPWCSStreamStatusFailed callback:^(FPWCSApi2Stream *rStream){
+        [self changeStreamStatus:rStream];
+        [self onUnpublished];
+    }];
+    if(![stream publish:&error]) {
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Failed to publish"
+                                     message:error.localizedDescription
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:@"Ok"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       [self onUnpublished];
+                                   }];
+        
+        [alert addAction:okButton];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    return stream;
+}
+
+//session and stream status handlers
+- (void)onConnected:(FPWCSApi2Session *)session {
+    [self publishStream];
+}
+
+- (void)onDisconnected {
+    [self changeViewState:_connectUrl enabled:YES];
+    [self onUnpublished];
+    if (url) {
+        //NSString *urlString = @"http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4";
+        NSString *urlString = [NSString stringWithFormat:@"http://%@:9091/client/records/%@", url.host, recordName];
+        _recordLink.text = urlString;
+        //[self playVideo: urlString];
+    }
+}
+
+- (void)onPublishing:(FPWCSApi2Stream *)stream {
+    [_startButton setTitle:@"STOP" forState:UIControlStateNormal];
+    [self changeViewState:_startButton enabled:YES];
+    recordName = [stream getRecordName];
+}
+
+- (void)onUnpublished {
+    [_startButton setTitle:@"START" forState:UIControlStateNormal];
+    [self changeViewState:_startButton enabled:YES];
+    [_remoteDisplay renderFrame:nil];
+}
+
+//user interface handlers
+
+- (void)startButton:(UIButton *)button {
+    [self changeViewState:button enabled:NO];
+    if ([button.titleLabel.text isEqualToString:@"STOP"]) {
+        if ([FPWCSApi2 getSessions].count) {
+            FPWCSApi2Session *session = [FPWCSApi2 getSessions][0];
+            NSLog(@"Disconnect session with server %@", [session getServerUrl]);
+            [session disconnect];
+        } else {
+            NSLog(@"Nothing to disconnect");
+            [self onDisconnected];
+        }
+    } else {
+        [self changeViewState:_connectUrl enabled:NO];
+        [self connect];
+    }
+}
+
+//status handlers
+- (void)changeConnectionStatus:(kFPWCSSessionStatus)status {
+    _status.text = [FPWCSApi2Model sessionStatusToString:status];
+    switch (status) {
+        case kFPWCSSessionStatusFailed:
+            _status.textColor = [UIColor redColor];
+            break;
+        case kFPWCSSessionStatusEstablished:
+            _status.textColor = [UIColor greenColor];
+            break;
+        default:
+            _status.textColor = [UIColor darkTextColor];
+            break;
+    }
+}
+
+- (void)changeStreamStatus:(FPWCSApi2Stream *)stream {
+    _status.text = [FPWCSApi2Model streamStatusToString:[stream getStatus]];
+    switch ([stream getStatus]) {
+        case kFPWCSStreamStatusFailed:
+            _status.textColor = [UIColor redColor];
+            break;
+        case kFPWCSStreamStatusPlaying:
+        case kFPWCSStreamStatusPublishing:
+            _status.textColor = [UIColor greenColor];
+            break;
+        default:
+            _status.textColor = [UIColor darkTextColor];
+            break;
+    }
+}
+
+//button state helper
+- (void)changeViewState:(UIView *)button enabled:(BOOL)enabled {
+    button.userInteractionEnabled = enabled;
+    if (enabled) {
+        button.alpha = 1.0;
+    } else {
+        button.alpha = 0.5;
+    }
+}
+
+//user interface views and layout
+- (void)setupViews {
+    //views main->scroll->content-videoContainer-display
+    _scrollView = [[UIScrollView alloc] init];
+    _scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+    _scrollView.scrollEnabled = YES;
+    
+    _contentView = [[UIView alloc] init];
+    _contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    _videoContainer = [[UIView alloc] init];
+    _videoContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    _remoteDisplay = [[RTCEAGLVideoView alloc] init];
+    _remoteDisplay.delegate = self;
+    _remoteDisplay.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    _connectUrl = [WCSViewUtil createTextField:self];
+    
+    _status = [WCSViewUtil createLabelView];
+    _startButton = [WCSViewUtil createButton:@"PLAY"];
+    [_startButton addTarget:self action:@selector(startButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _recordLink = [[UITextView alloc] init];
+    _recordLink.editable = NO;
+    _recordLink.dataDetectorTypes = UIDataDetectorTypeAll;
+    _recordLink.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    
+    [self.videoContainer addSubview:_remoteDisplay];
+    
+    [self.contentView addSubview:_videoContainer];
+    [self.contentView addSubview:_connectUrl];
+    [self.contentView addSubview:_status];
+    [self.contentView addSubview:_startButton];
+    [self.contentView addSubview:_recordLink];
+    
+//    _playerViewController = [[AVPlayerViewController alloc] init];
+//    _playerViewController.view.translatesAutoresizingMaskIntoConstraints = NO;
+//    [_playerViewController.view setFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width)];
+//    _playerViewController.showsPlaybackControls = YES;
+//    [self addChildViewController:_playerViewController];
+//    [self.contentView addSubview:_playerViewController.view];
+    
+    [self.scrollView addSubview:_contentView];
+    [self.view addSubview:_scrollView];
+    
+    //set default values
+    _connectUrl.text = @"wss://87.226.225.59:8443/test";
+}
+
+
+- (void)setupLayout {
+    NSDictionary *views = @{
+                            @"connectUrl": _connectUrl,
+                            @"status": _status,
+                            @"startButton": _startButton,
+                            @"remoteDisplay": _remoteDisplay,
+                            @"videoContainer": _videoContainer,
+                            @"recordLink": _recordLink,
+//                            @"videoPlayer": _playerViewController.view,
+                            @"contentView": _contentView,
+                            @"scrollView": _scrollView
+                            };
+    
+    NSNumber *videoHeight = @230;
+    //custom videoHeight for pads
+    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ) {
+        NSLog(@"Set video container height for pads");
+        videoHeight = @640;
+    }
+    
+    NSDictionary *metrics = @{
+                              @"linkHeight": @60,
+                              @"buttonHeight": @30,
+                              @"statusHeight": @30,
+                              @"labelHeight": @20,
+                              @"inputFieldHeight": @30,
+                              @"videoHeight": videoHeight,
+                              @"vSpacing": @15,
+                              @"hSpacing": @30
+                              };
+    
+    //constraint helpers
+    NSLayoutConstraint* (^setConstraintWithItem)(UIView*, UIView*, UIView*, NSLayoutAttribute, NSLayoutRelation, NSLayoutAttribute, CGFloat, CGFloat) =
+    ^NSLayoutConstraint* (UIView *dst, UIView *with, UIView *to, NSLayoutAttribute attr1, NSLayoutRelation relation, NSLayoutAttribute attr2, CGFloat multiplier, CGFloat constant) {
+        NSLayoutConstraint *constraint =[NSLayoutConstraint constraintWithItem:with attribute:attr1 relatedBy:relation toItem:to attribute:attr2 multiplier:multiplier constant:constant];
+        [dst addConstraint:constraint];
+        return constraint;
+    };
+    
+    void (^setConstraint)(UIView*, NSString*, NSLayoutFormatOptions) = ^(UIView *view, NSString *constraint, NSLayoutFormatOptions options) {
+        [view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:constraint options:options metrics:metrics views:views]];
+    };
+    
+    _remoteDisplayConstraints = [[NSMutableArray alloc] init];
+    
+    //set height size
+    setConstraint(_videoContainer, @"V:[videoContainer(videoHeight)]", 0);
+    setConstraint(_connectUrl, @"V:[connectUrl(inputFieldHeight)]", 0);
+    setConstraint(_status, @"V:[status(statusHeight)]", 0);
+    setConstraint(_startButton, @"V:[startButton(buttonHeight)]", 0);
+    setConstraint(_recordLink, @"V:[recordLink(linkHeight)]", 0);
+    //setConstraint(_playerViewController.view, @"V:[videoPlayer(videoHeight)]", 0);
+    
+    //set width related to super view
+    setConstraint(_contentView, @"H:|-hSpacing-[videoContainer]-hSpacing-|", 0);
+    setConstraint(_contentView, @"H:|-hSpacing-[connectUrl]-hSpacing-|", 0);
+    setConstraint(_contentView, @"H:|-hSpacing-[status]-hSpacing-|", 0);
+    setConstraint(_contentView, @"H:|-hSpacing-[startButton]-hSpacing-|", 0);
+    setConstraint(_contentView, @"H:|-hSpacing-[recordLink]-hSpacing-|", 0);
+    //setConstraint(_contentView, @"H:|-hSpacing-[videoPlayer]-hSpacing-|", 0);
+    
+    //remote display max width and height
+    setConstraintWithItem(_videoContainer, _remoteDisplay, _videoContainer, NSLayoutAttributeHeight, NSLayoutRelationLessThanOrEqual, NSLayoutAttributeHeight, 1.0, 0);
+    setConstraintWithItem(_videoContainer, _remoteDisplay, _videoContainer, NSLayoutAttributeWidth, NSLayoutRelationLessThanOrEqual, NSLayoutAttributeWidth, 1.0, 0);
+    
+    //remote display aspect ratio
+    NSLayoutConstraint *remoteARConstraint = setConstraintWithItem(_remoteDisplay, _remoteDisplay, _remoteDisplay, NSLayoutAttributeWidth, NSLayoutRelationEqual, NSLayoutAttributeHeight, 640.0/480.0, 0);
+    [_remoteDisplayConstraints addObject:remoteARConstraint];
+    
+    //position video views inside video container
+    setConstraint(_videoContainer, @"H:|[remoteDisplay]|", NSLayoutFormatAlignAllTop);
+    setConstraint(_videoContainer, @"V:|[remoteDisplay]|", 0);
+    
+//    setConstraint(self.contentView, @"V:|-50-[videoContainer]-vSpacing-[connectUrl]-vSpacing-[status]-vSpacing-[startButton]-vSpacing-[recordLink]-vSpacing-[videoPlayer]|", 0);
+    setConstraint(self.contentView, @"V:|-50-[videoContainer]-vSpacing-[connectUrl]-vSpacing-[status]-vSpacing-[startButton]-vSpacing-[recordLink]|", 0);
+    
+    //content view width
+    setConstraintWithItem(self.view, _contentView, self.view, NSLayoutAttributeWidth, NSLayoutRelationEqual, NSLayoutAttributeWidth, 1.0, 0);
+    
+    //position content and scroll views
+    setConstraint(self.view, @"V:|[contentView]|", 0);
+    setConstraint(self.view, @"H:|[contentView]|", 0);
+    setConstraint(self.view, @"V:|[scrollView]|", 0);
+    setConstraint(self.view, @"H:|[scrollView]|", 0);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - RTCEAGLVideoViewDelegate
+
+- (void)videoView:(RTCEAGLVideoView*)videoView didChangeVideoSize:(CGSize)size {
+    
+    NSLog(@"Size of remote video %fx%f", size.width, size.height);
+    [_remoteDisplay removeConstraints:_remoteDisplayConstraints];
+    [_remoteDisplayConstraints removeAllObjects];
+    NSLayoutConstraint *constraint =[NSLayoutConstraint
+                                     constraintWithItem:_remoteDisplay
+                                     attribute:NSLayoutAttributeWidth
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:_remoteDisplay
+                                     attribute:NSLayoutAttributeHeight
+                                     multiplier:size.width/size.height
+                                     constant:0.0f];
+    [_remoteDisplayConstraints addObject:constraint];
+    [_remoteDisplay addConstraints:_remoteDisplayConstraints];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+//- (void)playVideo:(NSString *)urlString {
+//    NSURL *url = [NSURL URLWithString:urlString];
+//    AVURLAsset *movieAsset = [AVURLAsset URLAssetWithURL:url options:nil];
+//    [movieAsset.resourceLoader setDelegate:self queue:dispatch_get_main_queue()];
+//    
+//    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:movieAsset];
+//    _player = [AVPlayer playerWithPlayerItem:playerItem];
+//    _playerViewController.player = _player;
+//    [_player play];
+//}
+
+// AVAssetResourceLoaderDelegate
+- (BOOL)resourceLoader:(AVAssetResourceLoader *)resourceLoader
+shouldWaitForResponseToAuthenticationChallenge:(NSURLAuthenticationChallenge *)authenticationChallenge
+{
+    //server trust
+    NSURLProtectionSpace *protectionSpace = authenticationChallenge.protectionSpace;
+    if ([protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
+    {
+        [authenticationChallenge.sender useCredential:[NSURLCredential credentialForTrust:authenticationChallenge.protectionSpace.serverTrust] forAuthenticationChallenge:authenticationChallenge];
+        [authenticationChallenge.sender continueWithoutCredentialForAuthenticationChallenge:authenticationChallenge];
+        
+    }
+    else{ // other type: username password, client trust..
+    }
+    return YES;
+}
+
+@end
