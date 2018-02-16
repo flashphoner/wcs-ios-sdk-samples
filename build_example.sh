e4eaddb0a473dca23b25753fd987a44e884caaba
Developer <developer@Developers-Mac.local>
2018-02-16 15:14:45 +0300

    Manual ProvisioningStyle

--- build_example.sh
+++ build_example.sh
@@ -10,7 +10,6 @@ done
 PROJECT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 CONFIGURATION="Release"
-PROVISIONING_PROFILE=$1
 
 rm -rf out-build
 mkdir out-build
@@ -19,37 +18,37 @@ mkdir out-build
 pod install
 
 #build and export
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "TwoWayStreaming" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoWayStreaming -archivePath out-build/TwoWayStreaming
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "TwoWayStreaming" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoWayStreaming -archivePath out-build/TwoWayStreaming
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/TwoWayStreaming.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "MediaDevices" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/MediaDevices -archivePath out-build/MediaDevices
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "MediaDevices" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/MediaDevices -archivePath out-build/MediaDevices
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/MediaDevices.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "Player" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Player -archivePath out-build/Player
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "Player" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Player -archivePath out-build/Player
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Player.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "TwoPlayers" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoPlayers -archivePath out-build/TwoPlayers
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "TwoPlayers" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoPlayers -archivePath out-build/TwoPlayers
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/TwoPlayers.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "Streamer" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Streamer -archivePath out-build/Streamer
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "Streamer" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Streamer -archivePath out-build/Streamer
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Streamer.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "StreamRecording" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/StreamRecording -archivePath out-build/StreamRecording
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "StreamRecording" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/StreamRecording -archivePath out-build/StreamRecording
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/StreamRecording.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "Conference" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Conference -archivePath out-build/Conference
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "Conference" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Conference -archivePath out-build/Conference
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Conference.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "VideoChat" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/VideoChat -archivePath out-build/VideoChat
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "VideoChat" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/VideoChat -archivePath out-build/VideoChat
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/VideoChat.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "PhoneMinVideo" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMinVideo -archivePath out-build/PhoneMinVideo
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "PhoneMinVideo" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMinVideo -archivePath out-build/PhoneMinVideo
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/PhoneMinVideo.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "PhoneMin" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMin -archivePath out-build/PhoneMin
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "PhoneMin" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMin -archivePath out-build/PhoneMin
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/PhoneMin.xcarchive -exportPath out-build
 
-xcodebuild -workspace 'WCSExample.xcworkspace' -scheme "ClickToCall" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/ClickToCall -archivePath out-build/ClickToCall
+xcodebuild PROVISIONING_PROFILE_SPECIFIER="$1" CODE_SIGN_IDENTITY="$2" -workspace 'WCSExample.xcworkspace' -scheme "ClickToCall" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/ClickToCall -archivePath out-build/ClickToCall
 xcodebuild -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/ClickToCall.xcarchive -exportPath out-build
 
 echo "Build complete"
