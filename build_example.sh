72832e31f50ff6f28bc322a6269d65ebf17e4e0e
Flashphoner <support@flashphoner.com>
2020-09-14 14:55:02 +0700

    Add plist generation with provisioning profile substitution to build examples #WCS-2445

--- build_example.sh
+++ build_example.sh
@@ -13,6 +13,47 @@ CONFIGURATION="Release"
 
 rm -rf ~/Library/Developer/Xcode/DerivedData/*
 
+# Prepare plist to build examples
+cat << EOF > Info_ipa.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>method</key>
+        <string>ad-hoc</string>
+        <key>signingStyle</key>
+        <string>manual</string>
+        <key>teamID</key>
+        <string>SXWV5Z47NK</string>
+        <key>provisioningProfiles</key>
+        <dict>
+               <key>com.flashphoner.ios.TwoWayStreaming</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.MediaDevices</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.Player</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.TwoPlayers</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.Streamer</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.StreamRecording</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.Conference</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.VideoChat</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.PhoneMinVideo</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.PhoneMin</key>
+               <string>$2</string>
+               <key>com.flashphoner.ios.ClickToCall</key>
+               <string>$2</string>
+        </dict>
+</dict>
+</plist>
+EOF
+
 rm -rf out-build
 mkdir out-build
 
@@ -20,38 +61,41 @@ mkdir out-build
 pod install
 
 #build and export
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "TwoWayStreaming" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoWayStreaming -archivePath out-build/TwoWayStreaming
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/TwoWayStreaming.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "TwoWayStreaming" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoWayStreaming -archivePath out-build/TwoWayStreaming
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/TwoWayStreaming.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "MediaDevices" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/MediaDevices -archivePath out-build/MediaDevices
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/MediaDevices.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "MediaDevices" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/MediaDevices -archivePath out-build/MediaDevices
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/MediaDevices.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Player" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Player -archivePath out-build/Player
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Player.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Player" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Player -archivePath out-build/Player
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/Player.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "TwoPlayers" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoPlayers -archivePath out-build/TwoPlayers
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/TwoPlayers.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "TwoPlayers" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/TwoPlayers -archivePath out-build/TwoPlayers
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/TwoPlayers.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Streamer" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Streamer -archivePath out-build/Streamer
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Streamer.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Streamer" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Streamer -archivePath out-build/Streamer
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/Streamer.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "StreamRecording" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/StreamRecording -archivePath out-build/StreamRecording
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/StreamRecording.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "StreamRecording" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/StreamRecording -archivePath out-build/StreamRecording
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/StreamRecording.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Conference" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Conference -archivePath out-build/Conference
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/Conference.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "Conference" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/Conference -archivePath out-build/Conference
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/Conference.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "VideoChat" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/VideoChat -archivePath out-build/VideoChat
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/VideoChat.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "VideoChat" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/VideoChat -archivePath out-build/VideoChat
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/VideoChat.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "PhoneMinVideo" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMinVideo -archivePath out-build/PhoneMinVideo
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/PhoneMinVideo.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "PhoneMinVideo" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMinVideo -archivePath out-build/PhoneMinVideo
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/PhoneMinVideo.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "PhoneMin" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMin -archivePath out-build/PhoneMin
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/PhoneMin.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "PhoneMin" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/PhoneMin -archivePath out-build/PhoneMin
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/PhoneMin.xcarchive -exportPath out-build
 
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "ClickToCall" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/ClickToCall -archivePath out-build/ClickToCall
-xcodebuild CODE_SIGN_STYLE=Automatic DEVELOPMENT_TEAM="$1" -allowProvisioningUpdates -exportArchive -exportOptionsPlist Info.plist -archivePath out-build/ClickToCall.xcarchive -exportPath out-build
+xcodebuild CODE_SIGN_STYLE=Automatic -UseModernBuildSystem=NO -workspace 'WCSExample.xcworkspace' -scheme "ClickToCall" -configuration="Release" clean archive OBJROOT=$(PWD)/out-build/ClickToCall -archivePath out-build/ClickToCall
+xcodebuild -exportArchive -exportOptionsPlist Info_ipa.plist -archivePath out-build/ClickToCall.xcarchive -exportPath out-build
+
+# Remove plist
+rm -rf Info_ipa.plist
 
 echo "Build complete"
 
